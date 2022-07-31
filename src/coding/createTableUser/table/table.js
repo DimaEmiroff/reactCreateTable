3035452d9f395c93024e6a8351d64ef78621e03d
dimonEmiroff <mr-tvister@mail.ru>
2022-07-31 15:20:46 +0200

    add_Button_Delete

--- src/coding/createTableUser/table/table.js
+++ src/coding/createTableUser/table/table.js
@@ -5,6 +5,7 @@ import TableRow from "../tableRow/tableRow";
 import classes from "./Table.module.scss";
 import sortedData from "../../sortedData/SortedData";
 
+
 export const field = [
     {id: 'Id', data:'id',sortable: false},
     {id:'Name',data:'name',sortable: true},
@@ -15,19 +16,33 @@ export const field = [
     {id:'On load', data:'on_load',sortable: false},
 ];
 
-const Table = ({ delData, onRemove }) => {
+
+const Table = () => {
+    const [delData, setDelData] = useState(arrUser)
     const [tableData, handleSorting] = sortedData(arrUser);
+
+    const handleDeleteClick = (id) => {
+        const removeData = [...delData];
+        // const index = delData.findIndex((data) => data.id === id);
+        const index = delData.filter((data) => data.id !== id);
+
+        // removeData.splice(index, 1);
+
+        setDelData(index);
+        console.log(index)
+
+    }
+
 
     return (
         <table className={classes.module_table}>
             <TableHead field={field} handleSorting={handleSorting}/>
-            {tableData.map((item, index) => (
+            {delData.map((item, index) => (
                 <TableRow key={item.id}
                           field={field}
-                          delData={delData}
                           item={item}
-                          onRemove={onRemove}
-                          tableData={tableData} />
+                          handleDeleteClick={handleDeleteClick}
+                          delData={delData} />
             ))}
         </table>
     );
--- src/coding/createTableUser/tableRow/tableRow.js
+++ src/coding/createTableUser/tableRow/tableRow.js
@@ -1,50 +1,19 @@
-import React, {useState} from 'react';
-import classes from "./Tbody.module.scss";
-
-import {arrUser} from "../../data/data";
-import ButtonDel from "../../IsMove/buttonDelete/ButtonDel";
+import React from 'react';
 
 
-const TableRow = ({ tableData, onRemove, item }) => {
-    const [delData, setDelData] = useState(arrUser)
-    const removeData = (index) => {
-        // const delRow = setDelData([...delData.slice(0, index), ...delData.slice(index - 1)]);
-        const delRow = delData.filter((item) => item.id !== index);
-        // delData.splice(index, 1);
-        setDelData(delRow)
-        // setDelData(delData.filter((item) => item !== index))
-            console.log(index)
-    }
-
+const TableRow = ({ item, handleDeleteClick }) => {
     return (
         <tbody>
-        {/*{tableData.map((item, index) => {*/}
-        {/*    return (*/}
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.phone}</td>
-                        <td>{item.email}</td>
-                        <td>{item.country}</td>
-                        <td>{item.age}</td>
-                        <td><button type="button" onClick={() => removeData(item.id)}>Delete</button></td>
-
-                        {/*--Сборка всех значений--*/}
-                        {/*{field.map(({data}) => {*/}
-                        {/*    const tData = item[data] ? item[data] : '-';*/}
-                        {/*    return <td*/}
-                        {/*        key={data}*/}
-                        {/*        className={classes.tbody_module}>*/}
-                        {/*        {tData}</td>;*/}
-                        {/*    })}*/}
-
-
-                        {/*<td><button type="button" onClick={() => removeData(item.id)}>Delete</button></td>*/}
-
-                    </tr>
-
-        {/*})}*/}
-        </tbody>
+            <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+                <td>{item.country}</td>
+                <td>{item.age}</td>
+                <td><button type={'button'} onClick={() => handleDeleteClick(item.id)}>remove</button></td>
+            </tr>
+         </tbody>
     );
 };
 
